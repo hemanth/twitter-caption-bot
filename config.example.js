e559b607ca6f3be3c343d2e0f21ef23681e29570
Hemanth.HM <hemanth.hm@gmail.com>
2016-05-05 23:55:17 +0530

    config.example

---
+++ config.example.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key:         'dZ6eVa1IS4yNoVZLjELFaf8K',
+    consumer_secret:      'y8lorBNN02BPtZDg2XNFqJKOxDxaOxZClIZGlYJqA51vjhXsw',
+    access_token_key:     '77134954501496832-m8jAxEyJ8SeYapPV4HRdoplSWZNWfWo',
+    access_token_secret:  'PspSQ6IX1cQmpLEj79CfqRVrrBwJVLQbzp0gcuXhLcV4',
+    timeout_ms:           60 * 1000,  // optional HTTP request timeout to apply to all requests.
+};
